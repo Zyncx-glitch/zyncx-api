f44270b10134540fb83e04458c1f9dacfb28c230
Zyncx-glitch <victorduarteriveros@gmail.com>
2026-04-08 20:06:01 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,8 +9,19 @@ def analizar():
     try:
         data = request.get_json()
         video_url = data.get('url')
+        solo_audio = data.get('solo_audio', False) # Recibe la orden de Godot
         
-        with yt_dlp.YoutubeDL({'quiet': True, 'noplaylist': True}) as ydl:
+        # Si es solo audio, buscamos el mejor formato de audio
+        # Si es video, buscamos el mejor formato que tenga audio y video juntos
+        format_str = 'bestaudio/best' if solo_audio else 'best[ext=mp4]/best'
+        
+        ydl_opts = {
+            'quiet': True, 
+            'noplaylist': True,
+            'format': format_str
+        }
+        
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
             return jsonify({
                 "status": "ok",
@@ -21,6 +32,5 @@ def analizar():
     except Exception as e:
         return jsonify({"status": "error", "msj": str(e)}), 400
 
-# Esta es la forma más simple que Render siempre entiende:
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
